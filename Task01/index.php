9825c89b8fffed7ef11ac441249306d908e343f3
Mr-HishamSaad <mregyeye@gmail.com>
2023-09-13 17:43:20 +0200

    Task 01 is Done!

--- Task01/index.php
+++ Task01/index.php
@@ -2,11 +2,36 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Task01</title>
+    <title>Tasko1 - Different Between echo and print</title>
 </head>
 <body>
+   
    <?
-        echo "PHP";
+    
+    #Variables 
+    
+    $fname = 'Hisham';
+    $lname = 'Mohamed';
+    
+    echo "My First name is $fname and my last name is $lname <br>";
+    
+    #Concatenation
+    
+    $full_name = $fname." ".$lname;
+    echo $full_name."<br>";
+    
+    
+    # Different Between ech and print
+    /*
+        1- echo is faster than print
+        2- print can return a value of 1 , While echo Not - So we can use print in expressions
+        3- echo can take multiple parameters wihle print can't
+    */
+    
+    echo "echo ", "can ", "take ", "multiple ", "parameters";
+    echo "<br>";
+    //print "print ", "can't ", "take ", "multiple ", "parameters";
+    
     ?>
     
 </body>
